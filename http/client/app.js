5651a07a871176be5be5cefb4e5cdd37a01dbb43
Leonel Radaelli <radaellileonel@gmail.com>
2020-01-06 21:26:45 -0300

    Comunicacion entre microservicios http

---
+++ http/client/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const {hello, helloClientSide, helloServerSide, helloBidirectional} = require('./http-client');
+
+const PORT = 4001;
+
+async function getHello(name) {
+    let response = await hello(name);
+    return ;
+}/*
+app.get('/serverSideStreamingRPC/', async function (req, res) {
+    let response = await helloServerSide(req, res);
+    return response;
+});
+
+app.get('/clientSideStreamingRPC/:name', async function (req, res) {
+    let response = await helloClientSide(req);
+    return res.send(response);
+});
+
+app.get('/bidirectionalStreamingRPC/:name', async function (req, res) {
+    let response = await helloBidirectional(req);
+    return res.send(response);
+});*/
+
+app.listen(PORT, function () {
+    console.log('Example app listening on port ' + PORT);
+    getHello("pepe");
+});
---
+++ http/client/http-client.js
@@ -0,0 +1,80 @@
+var request = require('request');
+
+
+function hello(name) {
+    let start =new Date();
+    console.log(start)
+    return request('http://localhost:5001/hello?name=' + name, function (error, response, body) {
+        //console.log('error:', error); // Print the error if one occurred
+        //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        let end=new Date();
+        console.log(end)
+        console.log(end-start)
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        return response;
+    });
+}
+
+function helloClientSide(req, res) {
+    /*   try {
+               var client = new greeting_proto.GreetingService('0.0.0.0' + ':50051',
+                   grpc.credentials.createInsecure());
+               return new Promise((resolve, reject) => {
+                       client.helloClientSide({},
+                           (error, response) => {
+                                   if (error) {
+                                           return reject(error);
+                                   }
+                                   console.log(response);
+                                   return resolve(response);
+                           });
+               });
+       } catch (e) {
+               console.error(e);
+       }*/
+}
+
+function helloServerSide(req, res) {
+    /*   try {
+               var client = new greeting_proto.GreetingServiceServerSide('0.0.0.0' + ':50051',
+                   grpc.credentials.createInsecure());
+               return new Promise(async (resolve, reject) => {
+                       let call = client.helloServerSide({});
+                       call.on('data', function(feature) {
+                               console.log(feature);
+                               res.send(feature)
+                       });
+                       /!*call.on('end', function() {
+                               res.send('Finalizo la comunicacion');
+                       });*!/
+               });
+       } catch (e) {
+               console.error(e);
+       }*/
+}
+
+function helloBidirectional(req, res) {
+    /* try {
+             var client = new greeting_proto.GreetingService('0.0.0.0' + ':50051',
+                 grpc.credentials.createInsecure());
+             return new Promise((resolve, reject) => {
+                     client.hello({},
+                         (error, response) => {
+                                 if (error) {
+                                         return reject(error);
+                                 }
+                                 console.log(response);
+                                 return resolve(response);
+                         });
+             });
+     } catch (e) {
+             console.error(e);
+     }*/
+}
+
+module.exports = {
+    hello,
+    helloClientSide,
+    helloServerSide,
+    helloBidirectional
+};
